d20e6f5f4466b6811381380d7ad4663bf2cc9c65
vlad <FreeMooont@gmail.com>
2021-03-17 19:22:59 +0800

    init commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router} from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
