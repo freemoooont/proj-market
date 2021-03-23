8d6e65d8ce5087f3829c3d6e9f46cf75c6c3c66f
vlad <FreeMooont@gmail.com>
2021-03-24 04:24:49 +0800

    Исправляет проблемы с иконками карточек. Чистит немного код.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useSelector, useDispatch} from "react-redux";
 import { Route } from 'react-router-dom'
 
 
-import Slider from './Component/Slider/Slider'
 import TopMenu from './Component/TopMenu/TopMenu'
 import UserMiniMenu from './Component/UI/UserMiniMenu/UserMiniMenu'
-import MainCard from './Component/ProjectCards/MainCard/MainCard'
+
 
 import { Home } from './pages'
 
