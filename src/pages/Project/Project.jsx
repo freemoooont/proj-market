165ed449207fc32a71d5577f6c4a88f20079ce06
Aalleexxaannddrr <alexandr.operman@gamil.com>
2021-04-09 14:45:56 +0800

    add people and deadlines

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ProjectHeader, ButtonUI, Deadlines} from "../../Component";
+import React, { Fragment } from "react";
+import { ProjectHeader, ButtonUI, Deadlines, People } from "../../Component";
 
 import { useParams } from "react-router-dom";
 
@@ -7,43 +7,48 @@ import "./project.css"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchProject } from "../../redux/actions/project";
 
-function Project(){
-    const {projId} = useParams();
+function Project() {
+    const { projId } = useParams();
 
     const dispatch = useDispatch();
-    const state = useSelector( ( {project} ) => project.items);
-    const isLoaded = useSelector(({project})=> project.isLoaded)
+    const state = useSelector(({ project }) => project.items);
+    const isLoaded = useSelector(({ project }) => project.isLoaded)
 
-    const author = isLoaded ? {...state.author} : "LOH PIDOR";
+    const author = isLoaded ? { ...state.author } : "LOH PIDOR";
     const desc = isLoaded ? state.desc : "hui, a ne desc";
     const deadlines = isLoaded ? state.deadlines : null;
+    const people = isLoaded ? state.people : null;
 
     console.log(desc)
-    React.useEffect(()=>{
-        dispatch(fetchProject(projId-1));
-    },[])
+    React.useEffect(() => {
+        dispatch(fetchProject(projId - 1));
+    }, [])
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
         console.log(isLoaded ? author.name : "yA pidORAS")
-    },[state])
+    }, [state])
 
     console.log(state);
-    return(
+    return (
         <div>
-            <ProjectHeader srcHeaderImg={state.img_header} name={author.name} brief={state.brief_desc} title={state.title} ico={author.ico}/>
-            <h2 className="project__desc--label">Описание проекта</h2>
-            {
-                isLoaded ?
-                desc.map((obj, idx)=>
-                    <p key={idx}>{obj.p}</p>
-                )
-                    : "PIDORAS EBANIY"
-            }
-            <ButtonUI txt={"Записаться на проект"} />
-            <img className="project--image" src={`/${state.img_side}`} />
-            <Deadlines deadlines={deadlines}/>
+            { isLoaded ?
+                <Fragment>
+                    <ProjectHeader srcHeaderImg={state.img_header} name={author.name} brief={state.brief_desc} title={state.title} ico={author.ico} />
+                    <h2 className="project__desc--label">Описание проекта</h2>
+                    {
+                        desc.map((obj, idx) =>
+                            <p key={idx}>{obj.p}</p>
+                        )
+                    }
+                    <ButtonUI txt={"Записаться на проект"} />
+                    <img className="project--image" src={`/${state.img_side}`} />
+                    <Deadlines deadlines={deadlines} />
+                    <People people={people} />
+                </Fragment>
+                : <div>sosat</div>}
+
         </div>
-)
+    )
 }
 
 export default Project;
